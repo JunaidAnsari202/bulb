1e1d7adcc2d1e31c2bf20f96cc4e4883f1f38bcf
junaidAnsari202 <junaidansari202goo@gmail.com>
2023-10-27 23:11:19 +0530

    first commit

---
+++ .history/script_20231027225620.js
@@ -0,0 +1,14 @@
+let button=document.getElementById('btn')
+    let bulb=document.getElementById('bulb')
+    button.addEventListener('click',bulbs)
+    function bulbs(e){
+        if(button.textContent.includes('on')){
+            bulb.src="img/off.jpg"
+             button.textContent="Turn on"
+            button.textContent="Turn off"
+        }
+        else{
+            bulb.src="img/on.jpg" 
+            button.textContent="Turn on"
+        }
+    }
